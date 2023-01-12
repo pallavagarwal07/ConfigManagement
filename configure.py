dd5efc8ca2280b0c819753dec27817ef484c76e9
Pallav Agarwal <pallav@even.in>
2023-01-13 01:18:44 +0530

    Some more minor changes

--- configure.py
+++ configure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import os
 import time
@@ -55,7 +55,7 @@ def codify(names):
     for i, n in enumerate(names):
         indices = [n.find(t) for t in tokens if n.find(t) != -1]
         if len(indices) == 0:
-            print "Statement %s contains no tokens" % n
+            print("Statement %s contains no tokens" % n)
             exit(1)
         names[i] = n[min(indices):]
     names = [c.split(' ') for c in names]
@@ -91,10 +91,10 @@ def start_symming(path):
         sym = os.path.expanduser(extract_path(open(path + "/folder_config").read().strip()))
         if os.path.exists(sym):
             if os.path.islink(sym):
-                print "Deleting symlink " + sym
+                print("Deleting symlink " + sym)
                 os.remove(sym)
             else:
-                print "Backuping file " + sym
+                print("Backuping file " + sym)
                 if not os.path.exists(backup):
                     os.makedirs(backup)
                 shutil.move(sym, backup)
@@ -103,7 +103,7 @@ def start_symming(path):
         except OSError as e: pass
         os.symlink(path, sym)
         sym_created.append(sym)
-        print "Symlink to " + path + " created."
+        print("Symlink to " + path + " created.")
     else:
         ls =[path + "/" + k for k in os.listdir(path)]
         for name in ls:
@@ -117,10 +117,10 @@ def start_symming(path):
                     sym = os.path.expanduser(sym)
                     if os.path.exists(sym):
                         if os.path.islink(sym):
-                            print "Deleting symlink " + sym
+                            print("Deleting symlink " + sym)
                             os.remove(sym)
                         else:
-                            print "Backuping file " + sym
+                            print("Backuping file " + sym)
                             if not os.path.exists(backup):
                                 os.makedirs(backup)
                             shutil.move(sym, backup)
@@ -129,22 +129,22 @@ def start_symming(path):
                     except OSError as e: pass
                     os.symlink(name, sym)
                     sym_created.append(sym)
-                    print "Symlink to " + name + " created."
+                    print("Symlink to " + name + " created.")
                 else:
-                    print "Orphaned file found at " + name
+                    print("Orphaned file found at " + name)
                     exit(1)
 
 state = open(get_path() + "/current_state").read()
 cur_files = [os.path.expanduser(k) for k in state.strip().split("\n")]
 backup = get_path() + "/backup/" + get_datetime()
-print "Backup folder: " + backup + "/\n"
+print("Backup folder: " + backup + "/\n")
 
 
 for file in cur_files:
     if os.path.islink(file):
-        print "Deleting symlink " + file
+        print("Deleting symlink " + file)
         os.remove(file)
 
 start_symming(get_path() + "/configs")
 open(get_path() + "/current_state", "w").write("\n".join(sym_created))
-print "\nState successfully witten to current_state"
+print("\nState successfully witten to current_state")
